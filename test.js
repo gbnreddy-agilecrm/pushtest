0ab4d8b761148e2eee3be0d1408e046379096b71
geethanjali-1996 <52911559+geethanjali-1996@users.noreply.github.com>
2019-08-14 08:58:00 -0400

    updated with debugger in test

--- test.js
+++ test.js
@@ -1,6 +1,6 @@
 const publicVapidKey =
  "BJthRQ5myDgc7OSXzPCMftGw-n16F7zQBEN7EUD6XxcfTTvrLGWSIG7y_JxiWtVlCFua0S8MTB5rPziBqNx1qIo";
-
+debugger;
   // Declaring the dynamic variables
 
 // Check for service worker
